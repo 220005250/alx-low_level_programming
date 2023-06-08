0b34bea6592dc52b720bee00abfb40829540c191
Sephorah Koko Koto Tshitenge <kstshitenge@gmail.comm>
2023-06-08 05:38:10 +0200

    0x05: Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98	-  funcction that takes a pointer to an int
+ *							as parameter and updates the value it points to to 98.
+ *
+ * @n: pointer to an integer
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef MAINH
+
+void reset_to_98(int *n);
+
+#endif
