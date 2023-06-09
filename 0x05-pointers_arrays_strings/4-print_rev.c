1ef54ef2f4311e33e13d8473acdffc314b61164f
Sephorah Koko Koto Tshitenge <kstshitenge@gmail.comm>
2023-06-09 23:00:26 +0200

    0x05:Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+
+#include "main.h"
+
+/**
+ * print_rev - function prints a string, in reverse, followed by a new line.
+ *@s : pointer to a string
+ *Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+int i, j, len;
+i = 0;
+while (s[i] != '\0')
+{
+	i++;
+}
+len = i;
+for (j = len - 1; j >= 0; j--)
+{
+	_putchar(s[j]);
+}
+_putchar('\n');
+}
